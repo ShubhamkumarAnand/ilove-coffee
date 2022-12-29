edeea8dd1051223b70e8a915b29f9f4ff50e5695
imskanand <the.anand3099@gmail.com>
2022-12-29 21:43:25 +0530

    Dynamic Param Routes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavours')
   findAll() {
     return 'This returns all the coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This returns the ${id} from the Parameter`;
   }
 }

d8bfa51379e552a2d3f7159f120f8ff47bd09dce
imskanand <the.anand3099@gmail.com>
2022-12-30 18:24:02 +0530

    Creating a POST Request

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavours')
+  @Get()
   findAll() {
     return 'This returns all the coffees';
   }
@@ -10,5 +10,10 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This returns the ${id} from the Parameter`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
